9bcb5529cde683dd54c3cad2e7c574d739d1ee77
Arren <akleung@scu.edu>
2021-12-07 22:24:04 -0800

    getting there!!!

--- mongodb/UserCollection.js
+++ mongodb/UserCollection.js
@@ -6,23 +6,6 @@ const User = require("../models/User");
 const UserCollection = (db) => {
     const collection = db.collection("users");
 
-    const getUser = (id) => {
-        let filter;
-
-        try {
-            filter = { _id: new mongodb.ObjectId(id) };
-        } catch (err) {
-            return Promise.reject({ code: "INVALID_ID" });
-        }
-
-        return collection.findOne(filter).then((user) => {
-            if (!user) {
-                return Promise.reject({ code: "NOT_FOUND" });
-            }
-            return { user };
-        });
-    };
-
     const getAllUsers = () => {
         return collection
             .find()
@@ -40,24 +23,9 @@ const UserCollection = (db) => {
         });
     };
 
-    const updateScore = (id, score) => {
-        const filter = { _id: new mongodb.ObjectId(id) };
-        return collection
-            .updateOne(filter, {
-                $set: {
-                    highscore: score,
-                },
-            })
-            .then(() => {
-                return collection.findOne(filter);
-            });
-    };
-
     return {
-        getUser,
         getAllUsers,
         createUser,
-        updateScore,
     };
 };
 
